8ca4632af2c529bd1e81f8337e27d41010aa6653
DaveCorleto <davide.corleto@fastwebnet.it>
2024-01-13 15:54:49 +0100

    feat getcallTMDBImageinMovies

--- src/App.vue
+++ src/App.vue
@@ -41,6 +41,27 @@ export default {
           console.log("Errori", err)
         }
         )
+    },
+    // Voglio creare un secondo metodo: se l'utente ha fatto una scelta ed è stato valorizzato l'array Movies  allora lo chiamo: 
+    getcallTMDBImageinMovies() {
+
+      // Mi creo una variabile per catturare la richiesta dell'utente. Se ha effettuato una ricerca allora myUrlMovies cambierà 
+      let myUrlImageMovies = `https://api.themoviedb.org/3/movie/${store.UserSearch[0].id}/images?include_image_language=en&language=string&api_key=8577ddfe1686e0538f479ffca7d3424e`;
+
+      if (store.Movies.length > 0) {
+        myUrlImageMovies += `&query=${store.UserSearch}`
+      }
+      axios
+        .get(myUrlImageMovies)
+        .then((res => {
+          console.log("Questo è l'elenco delle immagini che mi ritorna da myUrlImageMovies", res.data);
+          store.Movies = res.data.results;
+        }))
+
+        .catch((err) => {
+          console.log("Errori", err)
+        }
+        )
     }
   }
 
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -17,11 +17,13 @@ export default {
 
 <template>
     <div>Sono AppMain</div>
-    <div v-if="store.Movies !== ''" class="prova">
-        <div>{{ store.Movies[0].title }}</div>
-        <div>{{ store.Movies[0].original_title }}</div>
-        <div>{{ store.Movies[0].original_language }}</div>
-        <div>{{ store.Movies[0].vote_average }}</div>
+    <div v-if="store.Movies.length > 0" class="prova">
+        <div>Questo è il titolo: {{ store.Movies[0].title }}</div>
+        <div>Questo è il titolo originale {{ store.Movies[0].original_title }}</div>
+        <div>Questa è la lingua {{ store.Movies[0].original_language }}</div>
+        <div>Votazione film {{ store.Movies[0].vote_average }}</div>
+        <div><img :src="store.Movies[0].backdrop_path" alt=""></div>
+        <div>Id del film {{ store.Movies[0].id }}</div>
     </div>
 </template>
 
